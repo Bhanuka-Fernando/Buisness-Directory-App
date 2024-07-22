577682a89cae0f4dc3b00a46bf3be90b09d2d4b9
Bhanuka-Fernando <bhanukafernando2@gmail.com>
2024-07-22 13:36:43 +0530

    Application is created

---
+++ Buisness-directory/app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+
+      }}
+    >
+      <Text>Bhanuka is the best coder</Text>
+    </View>
+  );
+}
